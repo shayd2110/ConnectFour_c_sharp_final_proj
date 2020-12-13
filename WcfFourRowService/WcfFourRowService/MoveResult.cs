865700e47f76d05d6a7213651a17bb20692b4120
Shay Doron <shayd2110@gmail.com>
2020-12-13 11:54:09 +0200

    Final Vesrion

---
+++ WcfFourRowService/WcfFourRowService/MoveResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+/*WcfFourRowService namespace*/
+namespace WcfFourRowService
+{
+    /*MoveResult enum*/
+    /*credit to - dani's code*/
+    /// <summary>
+    /// enum that represent result of the four row game
+    /// </summary>
+    [DataContract]
+    public enum MoveResult
+    {
+        [EnumMember] YouWon,
+        [EnumMember] YouLose,
+        [EnumMember] Draw,
+        [EnumMember] NotYourTurn,
+        [EnumMember] GameOn,
+        [EnumMember] Nothing,
+        [EnumMember] IllegalMove,
+
+    }/*end of -MoveResult- enum*/
+
+}/*end of -WcfFourRowService- namespace*/
---
+++ WcfFourRowService/WcfFourRowService/UserDoesntExistsFault.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+/*WcfFourRowService namespace*/
+namespace WcfFourRowService
+{
+    /*UserDoesntExistsFault class*/
+    /// <summary>
+    /// some fault exception
+    /// </summary>
+    [DataContract]
+    public class UserDoesntExistsFault
+    {
+        [DataMember]
+        public string Details { get; set; }
+
+    }/*end of -UserDoesntExistsFault- class*/
+
+}/*end of -WcfFourRowService- namespace*/
